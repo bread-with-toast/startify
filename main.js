108b4726a59732bdbf7c1a81d57942723043f0e8
bread-with-toast <108555960+bread-with-toast@users.noreply.github.com>
2022-09-09 22:12:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var identifier = document.getElementById("identifier")
+var minute = document.getElementById("ideas-min")
+var fixed = document.getElementById("ideas-fix")
+var goals = document.getElementById("goals")
+var detail = document.getElementById("details")
+
+function save() {
+    localStorage.setItem("finder" + identifier.value, identifier.value)
+    localStorage.setItem(identifier.value + "Minute", minute.value)
+    localStorage.setItem(identifier.value + "Fixed", fixed.value)
+    localStorage.setItem(identifier.value + "Goals", goals.value)
+    localStorage.setItem(identifier.value + "Detail", detail.value)
+}
+
+function get() {
+    document.getElementById("name").innerText = localStorage.getItem("finder" + identifier.value)
+    document.getElementById("mins").innerText = localStorage.getItem(identifier.value + "Minute")
+    document.getElementById("fixs").innerText = localStorage.getItem(identifier.value + "Fixed")
+    document.getElementById("goalss").innerText = localStorage.getItem(identifier.value + "Goals")
+    document.getElementById("detailss").innerText = localStorage.getItem(identifier.value + "Detail")
+}
+
+function del() {
+    localStorage.removeItem("finder" + identifier)
+    localStorage.removeItem(identifier.value + "Minute")
+    localStorage.removeItem(identifier.value + "Fixed")
+    localStorage.removeItem(identifier.value + "Goals")
+    localStorage.removeItem(identifier.value + "Detail")
+}
